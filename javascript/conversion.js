5269518fe10a0fe89e430a23c0a68cc08574ca4c
nehalkal <124927313+nehalkal@users.noreply.github.com>
2024-09-21 17:40:10 +0000

    conversion in js

--- javascript/conversion.js
+++ javascript/conversion.js
@@ -36,7 +36,56 @@
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(3-2);
+// console.log(3+2);
+// console.log(3*2);
+// console.log(3**2); // 3 to the power 2
+// console.log(3/2);
+// console.log(3%2);
+
+let str1 = "Chin Tapak";
+let str2 = " Dam Dam";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((5 + 8) - 3 * 2 % 8);
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");     // same try for the false
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2; // Wrong way to write
+
+// let gameCounter = 100;
+// gameCounter++;    // ++gameCounter => Both are the same
+// console.log(gameCounter);
+
+// let x = 3;
+// const y = x++;
+// console.log('x:${x}, y:${y}');  // Print As it is 
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+// console.log(`a = ${a}, b = ${b}`);
 
