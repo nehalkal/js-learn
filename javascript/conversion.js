76131f5d69881384f6ad71fbdc44b249b09ee24b
nehalkal <124927313+nehalkal@users.noreply.github.com>
2024-09-21 11:15:23 +0000

    conversion of datatypes

---
+++ javascript/conversion.js
@@ -0,0 +1,42 @@
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let scorar = "34";
+// console.log(typeof scorar);
+// console.log(typeof (scorar));
+
+// let scoree = "35abc";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =. NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "Nehal";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
